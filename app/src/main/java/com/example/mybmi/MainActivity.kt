5e8e21c86a4286d96b23283fd963b5b492b4779a
IT <kph7mgb@gmail.com>
2019-10-16 02:24:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mybmi/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mybmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+//inputアイコンは、MITライセンス　：https://icons-for-free.com/user+icon-1320166904740390564/
+//履歴アイコンは、CCライセンス　：https://icon-icons.com/ja/%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3/%E5%B1%A4/64757
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
