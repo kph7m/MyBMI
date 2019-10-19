517a3afa6fb5ed0a2c77649a89540bfca53948b8
IT <kph7mgb@gmail.com>
2019-10-20 03:11:34 +0900

    LGTM

--- app/src/main/java/com/example/mybmi/MainActivity.kt
+++ app/src/main/java/com/example/mybmi/MainActivity.kt
@@ -6,18 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import androidx.core.content.edit
+import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_input.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.math.round
 
-class MainActivity : Activity() {
+class MainActivity : FragmentActivity() {
 //inputアイコンは、MITライセンス　：https://icons-for-free.com/user+icon-1320166904740390564/
 //履歴アイコンは、CCライセンス　：https://icon-icons.com/ja/%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3/%E5%B1%A4/64757
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         //INPUT項目の初期値登録
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
